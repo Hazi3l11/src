d7e1f31c90b17e65fcaa690180694011f1b7c288
holahola11 <holamundi@email.com>
2026-02-06 08:02:04 -0600

    Se definieron los endpoints

--- crud.py
+++ crud.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
-app = FastAPI
+app = FastAPI()
 
 class Cliente(BaseModel):
     id:int
@@ -18,3 +18,20 @@ class Servicio(BaseModel):
     id:int
     descripcion:str
     costo: float
+
+cliente: List [Cliente] = []
+vehiculo: list [Vehiculo] = []
+servicio: list [Servicio] = []
+
+#Definir los Endpoints
+@app.get("/clientes", response_model=List[Cliente])
+def mostrar_clientes():
+    return cliente
+
+@app.get("/vehiculos", response_model=List[Vehiculo])
+def mostrar_clientes():
+    return vehiculo
+
+@app.get("/servicios", response_model=List[Servicio])
+def mostrar_clientes():
+    return servicio
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
-app = FastAPI
+app = FastAPI()
 
 #Modelos
 class Cliente(BaseModel):
